4f12f0fec7b28fef73cc67b91343e051b40ba4e2
DESKTOP-GV16N45\Alumno <informatica@esne.es>
2025-02-10 15:14:37 +0100

    add base snake

---
+++ Back-end/C++/ejEnClase/ejAccesoAFicheros.cpp
@@ -0,0 +1,26 @@
+// Bueno esto ya lo conozco pero es que esoty en clas ey tengo que apuntarlo
+
+#include <iostream>
+#include <fstream>
+int main(){
+    // ifstream file;
+    file.open("Ruta de donde esta el fichero");
+    if(file.is_open(){
+
+    })
+    file.close();
+
+    // Otra forma seria
+
+    // ifstream file(path);
+
+    // ifstream lectura de datos
+    // ofstream escritura de datos
+    // fsteam generico
+
+    // String linea
+    // while ( getline(fichero,linea))
+    // std::cout << linea << endl;
+    // while(get(file.get(char)))
+    return 0;
+}
