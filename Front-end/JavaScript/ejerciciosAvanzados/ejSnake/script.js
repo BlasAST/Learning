943df25cfa2612e2debd3b0731b62c8249c087b3
BlasAST <blas.siguencia1@gmail.com>
2025-02-14 20:50:25 +0100

    little commit

--- Front-end/JavaScript/ejerciciosAvanzados/ejSnake/script.js
+++ Front-end/JavaScript/ejerciciosAvanzados/ejSnake/script.js
@@ -1,9 +1,21 @@
 document.addEventListener("DOMContentLoaded",()=>{
     main();
+    let snake= document.querySelector("head");
+    
 })
 
 function main(){
     window.addEventListener("keydown",(event)=>{
-        console.log(event);
+        event.key=="ArrowUp"?console.log("Arriba"):
+        event.key=="ArrowDown"?console.log("Abajo"):
+        event.key=="ArrowRight"?console.log("Derecha"):
+        event.key=="ArrowLeft"?console.log("Izquierda"):null;
     })
+}
+
+function mover(X,Y){
+    setInterval(()=>{
+        let cadena= `left: ${X} px;top: ${Y};transform:rotate();`
+        snake.setAttribute("style",cadena);
+    },2000)
 }
