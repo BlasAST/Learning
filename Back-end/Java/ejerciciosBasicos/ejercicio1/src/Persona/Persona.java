9d60444c50b136919d8cbbcc17082a8168649579
BlasAST <blas.siguencia1@gmail.com>
2025-05-06 00:47:06 +0200

    service java

---
+++ Back-end/Java/ejerciciosBasicos/ejercicio1/src/Persona/Persona.java
@@ -0,0 +1,27 @@
+
+package Persona;
+
+public class Persona {
+    public String Nombre;
+    public int Edad;
+    public String Apellidos;
+    
+    public Persona(String nombre, int edad, String apellidos) {
+        Nombre = nombre;
+        Edad = edad;
+        Apellidos = apellidos;
+    }
+
+    public String getNombre() {
+        return Nombre;
+    }
+
+    public int getEdad() {
+        return Edad;
+    }
+
+    public String getApellidos() {
+        return Apellidos;
+    }
+    
+}
